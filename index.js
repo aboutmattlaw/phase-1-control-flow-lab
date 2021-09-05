cbf7f4c223512f87d74035bf9c4df6bed168ec25
Matt <aboutmattlaw@gmail.com>
2021-09-05 16:17:31 -0400

    commit tree 7ae69ec15f87a3046f7664c79f6e90eda5a4e557

--- index.js
+++ index.js
@@ -1,11 +1,87 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if(distance <= 400){
+    return 'This one is on me!';
+  } else if(1999 < distance && distance < 2500){
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// First Lab Attempt 
+
+// function scuberGreetingForFeet(distance){
+//   // Write your code here!
+//     if(distance <=400){
+//       return 'This one is on me!'
+//     } else if (1999 < distance && distance < 2500){
+//       return "I will gladly take your thirty bucks."
+//       } else {
+//         return "No can do."
+//       }
+//     }
+
+// function ternaryCheckCity(city){
+//   // Write your code here!
+//   return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+// }
+
+// function switchOnCharmFromTip(tip){
+//   // Write your code here!
+//     switch (tip) {
+//       case 'generous':
+//         return 'Thank you so much.'
+//         break;
+//       case 'not as generous':
+//         return 'Thank you.'
+//       default:
+//         return 'Bye.'
+//     }
+//   }
